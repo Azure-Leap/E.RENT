30f4a130115a40176becb21716476b174cd07677
MuugiiU <munkhtsetsegurtnasan@Munkhtsetsegs-MacBook-Pro.local>
2023-05-15 12:38:08 +0800

    chat zasav

--- front_end/src/components/Realtime_chat/index.tsx
+++ front_end/src/components/Realtime_chat/index.tsx
@@ -82,7 +82,7 @@ const Realtime = () => {
                     <div className="flex items-center justify-start flex-row-reverse">
                       <div className="flex items-center justify-center h-10 w-10 rounded-full bg-indigo-500 flex-shrink-0">A</div>
                       <div className="relative mr-3 text-sm bg-indigo-100 py-2 px-4 shadow rounded-xl">
-                        <div>I'm ok what about you?</div>
+                        <div>I&#39;m ok what about you?</div>
                       </div>
                     </div>
                   </div>
