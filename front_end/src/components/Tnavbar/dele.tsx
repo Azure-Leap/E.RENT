ade77ef45c1f937dafb1b792b6e125e339533134
ekoerkhe <ekoerkhe@ekoerkhes-MacBook-Pro.local>
2023-05-23 18:38:44 +0800

    etssiin

--- front_end/src/components/Tnavbar/dele.tsx
+++ front_end/src/components/Tnavbar/dele.tsx
@@ -43,20 +43,6 @@ const Dele = () => {
             <Image src={erent} alt="pic" className="w-20" />
           </div>
 
-<<<<<<< Updated upstream
-          <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto ">
-            <div className=" lg:flex-grow">
-              {/* <Link
-                href="/"
-                className="block mt-4 lg:inline-block lg:mt-0 text-gray-600 font-medium text-lg hover:text-gray-400 mr-4"
-              >
-                Нүүр
-              </Link> */}
-
-              <Link href="/services" className="block mt-4 lg:inline-block lg:mt-0 text-gray-600 font-medium text-lg hover:text-gray-400">
-                Үйлчилгээ
-              </Link>
-=======
           <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div className="lg:flex-grow">
               <div className="flex justify-between">
@@ -83,7 +69,6 @@ const Dele = () => {
                   </button>
                 </Link>
               </div>
->>>>>>> Stashed changes
             </div>
 
             <Link href={"/Cart"}>
@@ -285,55 +270,43 @@ const Dele = () => {
         </div>
         <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformdropdownAvatarNameButtonationButton">
           <li>
-<<<<<<< Updated upstream
             <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-=======
             <Link
               href="#"
               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
->>>>>>> Stashed changes
               Dashboard
             </Link>
           </li>
           <li>
-<<<<<<< Updated upstream
             <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-=======
             <Link
               href="#"
               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
->>>>>>> Stashed changes
               Settings
             </Link>
           </li>
           <li>
-<<<<<<< Updated upstream
             <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-=======
             <Link
               href="#"
               className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
             >
->>>>>>> Stashed changes
               Earnings
             </Link>
           </li>
         </ul>
         <div className="py-2">
-<<<<<<< Updated upstream
           <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white " onClick={handleOut}>
             Гарах
           </a>
-=======
           <Link
             href="#"
             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
           >
             Sign out
           </Link>
->>>>>>> Stashed changes
         </div>
       </div> */}
     </>
