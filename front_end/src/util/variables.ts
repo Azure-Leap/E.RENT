f99c33ebf66b9b0d30a9ce8e9ee17b1c24d4a589
MuugiiU <classroom2leap@gmail.com>
2023-05-26 01:08:52 +0800

    change var

--- front_end/src/util/variables.ts
+++ front_end/src/util/variables.ts
@@ -1,1 +1,1 @@
-export const BASE_URL_API = "http://localhost:9000"; //https://erent.onrender.com
+export const BASE_URL_API = "https://erent.onrender.com"; //https://erent.onrender.com http://localhost:9000
